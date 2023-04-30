9ca2b007bfbf723ccd74107d8476fd600c9c5350
joanfrostjoanlee <linakelp@gmail.com>
2023-04-30 12:55:14 +0300

    Finišo langas
    Pridėtas žaidimą baigus pasveikinantis langas

---
+++ Assets/Scripts/Menus/Finish.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    //Scriptas paspaudus bet kurį mygtuką grąžiną žaidėją į pagrindinį meniu
+
+    Scene currentScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.anyKeyDown && !Input.GetKey(KeyCode.UpArrow)) || (Time.timeSinceLevelLoad > 1 && Input.anyKeyDown))
+            SceneManager.LoadScene("MainMenu");
+    }
+}
